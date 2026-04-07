e0eadd30bda25f5a0f52073773d23ad6a8a3ee1d
MMDDKK6500 <joao.dnwandermuren@senacsp.edu.br>
2026-04-07 17:44:43 -0300

    Commit inicial

---
+++ TopazStay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TopazStay
+//
+//  Created by JOAO DUQUE NARDELLI WANDERMUREN on 07/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TopazStay/TopazStayApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopazStayApp.swift
+//  TopazStay
+//
+//  Created by JOAO DUQUE NARDELLI WANDERMUREN on 07/04/26.
+//
+
+import SwiftUI
+
+@main
+struct TopazStayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
