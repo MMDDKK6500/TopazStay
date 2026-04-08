fd563bd77b547021d3f83935abd4174afed22cea
Felipe <felipepro1@hotmail.com>
2026-04-08 15:45:32 -0300

    Criação de tela de cadastro

---
+++ TopazStay/TelaCadastro.swift
@@ -0,0 +1,18 @@
+//
+//  TelaCadastro.swift
+//  TopazStay
+//
+//  Created by FELIPE COLARES CARDOSO on 08/04/26.
+//
+
+import SwiftUI
+
+struct TelaCadastro: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    TelaCadastro()
+}
