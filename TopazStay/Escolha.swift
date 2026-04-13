cc080f8721cb872bd64ec0922b2265f7b7df59e1
Felipe <felipepro1@hotmail.com>
2026-04-13 16:27:10 -0300

    Criação de tela (sheet) de escolha de quartos e componentes.

---
+++ TopazStay/Escolha.swift
@@ -0,0 +1,178 @@
+//
+//  Escolha.swift
+//  TopazStay
+//
+//  Created by FELIPE COLARES CARDOSO on 13/04/26.
+//
+
+import SwiftUI
+
+struct Escolha: View {
+   // @State private var showSheet: Bool = false
+    @State private var offset:CGFloat = 60
+    @State private var lastOffset: CGFloat = 0
+    
+    var body: some View {
+        
+        GeometryReader { geometry in
+                    
+                    let screenHeight = geometry.size.height
+                    let minHeight = screenHeight / 6
+            let maxHeight = screenHeight / 1.4
+                    
+                
+                    ZStack(alignment: .bottom) {
+                       
+                        VStack {
+                         
+                                 
+                            Capsule()
+                                .frame(width: 85, height: 6)
+                                .foregroundColor(.white)
+                                .padding(.top, 8)
+                            Text("Total de reservas")
+                              .font(
+                                Font.custom("Poppins", size: 20)
+                                  .weight(.semibold)
+                              )
+                              .foregroundColor(.white.opacity(0.7))
+                              .frame(width: 245, height: 24, alignment: .topLeading)
+                              .padding(.top, 15)
+                              .padding(.leading, -90)
+                            
+                            Text("R$ 1550,00") // preço variável
+                              .font(
+                                Font.custom("Poppins", size: 20)
+                                  .weight(.medium)
+                              )
+                              .foregroundColor(.white)
+                              .frame(width: 108, height: 20, alignment: .topLeading)
+                              .padding(.top, -30)
+                              .padding(.leading, 200)
+                            ZStack{
+                                Rectangle()
+                                    .frame(width: 350, height: 385)
+                                    .background(Color.white)
+                                    .colorInvert()
+                                    .cornerRadius(20)
+                                    .padding(.top, 40)
+                                
+                                // Conteúdo da escolha de quartos.
+                                
+                                ScrollView(.vertical){
+                                    VStack(spacing: -10){ // espaçamento
+                                        ForEach(0..<3){ // quantidade de quartos variável
+                                            card in CardEscolha()
+                                        }
+                                    }
+                                    
+                                }
+                                .padding(.top, 40)
+                                .frame(width: 350, height: 421)
+                            }
+                            Button(action: { print("Botao confirmar clicado ")}){
+                                Rectangle()
+                                    .frame(width: 183, height: 36)
+                                    .foregroundColor(Color.botaoFiltros)
+                                    .cornerRadius(10)
+                            }
+                                    .padding(.top, 15)
+                                    .padding(.leading, 150)
+                                Text("Confirmar reserva")
+                                    .font(
+                                        Font.custom("Poppins", size: 15)
+                                            .weight(.medium)
+                                    )
+                                    .foregroundColor(.white)
+                                    .padding(.top, -35)
+                                    .padding(.leading, 150)
+                                // ZStack {   .frame(width: 393, height: 520) }
+                           
+                           
+                            Spacer()
+                        }
+                        .frame(height: 700)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.menuInferiorSuperior)
+                        .clipShape(
+                            UnevenRoundedRectangle(
+                                topLeadingRadius: 20,
+                                bottomLeadingRadius: 0,
+                                bottomTrailingRadius: 0,
+                                topTrailingRadius: 20
+                            )
+                        )
+                        .shadow(radius: 10)
+                        .offset(y: screenHeight - minHeight + offset)
+                        .gesture(
+                            DragGesture()
+                                .onChanged { value in
+                                    let newOffset = value.translation.height + lastOffset
+                                    
+                                    // Limita entre aberto e fechado
+                                    if newOffset <= 0 && newOffset >= -(maxHeight - minHeight) {
+                                        offset = newOffset
+                                    }
+                                }
+                                .onEnded { _ in
+                                    
+                                    // Se arrastou mais da metade, abre
+                                    if -offset > (maxHeight - minHeight) / 1.4 {
+                                        offset = -(maxHeight - minHeight)
+                                    } else {
+                                        offset = 60
+                                    }
+                                    
+                                    lastOffset = offset
+                                }
+                        )
+                    }
+                    
+                }//safeAreaInset(edge: .bottom)
+    
+    }//.safeAreaInset(edge: .bottom)
+}
+
+#Preview {
+    Escolha()
+    
+    
+    /*  @Previewable let hotel: Hotel = Hotel(
+     nome: "Hotel dos bão",
+     local: "aqui",
+     avaliacao: 5,
+     descricao: "Hotel muito bom com uma descrição muito boa que nunca acaba pois esse hotel realmente é muito muito bom ta?\n\nTipo, muito bom mesmo mano, c não tem IDEIA do quão bom esse hotel é mano, slg, se chama copacaban palace, e vc NUNCA vai adivinhar isso... fica em copacabana",
+     imagem: "imagens/hoteis/2",
+     favorito: true,
+     quartos: [
+         Quarto(
+             nome: "Padrão",
+             descricao: "Descrição genial",
+             maxPessoas: 2,
+             valor: 350,
+             imagem: "imagens/quartos/8"
+         ),
+         Quarto(nome: "Padrão", descricao: "Descrição genial", maxPessoas: 2, valor: 350, imagem: "imagens/quartos/8"),
+         Quarto(nome: "Padrão", descricao: "Descrição genial", maxPessoas: 2, valor: 350, imagem: "imagens/quartos/8"),
+         Quarto(nome: "Padrão", descricao: "Descrição genial", maxPessoas: 2, valor: 350, imagem: "imagens/quartos/8")],
+     comentarios: [
+         Comentario(
+             foto: "logo",
+             nome: "Guilherme",
+             avaliacao: 4,
+             texto: "Hotel incrível, recomendo muito!dsadsdsadsadsadsadsdasdsadsadasdsadsadasd",
+         ),
+         Comentario(
+             foto: "logo",
+             nome: "Guilherme",
+             avaliacao: 4,
+             texto: "Hotel incrível, recomendo muito!dsadsdsadsadsadsadsdasdsadsadasdsadsadasd",
+         ),
+         Comentario(
+             foto: "logo",
+             nome: "Guilherme",
+             avaliacao: 4,
+             texto: "Hotel incrível, recomendo muito!dsadsdsadsadsadsadsdasdsadsadasdsadsadasd",
+         )
+     ])*/
+}
---
+++ TopazStay/componentes/CardEscolha.swift
@@ -0,0 +1,163 @@
+//
+//  CardEscolha.swift
+//  TopazStay
+//
+//  Created by FELIPE COLARES CARDOSO on 13/04/26.
+//
+
+import SwiftUI
+
+struct CardEscolha: View {
+    var body: some View {
+        Rectangle()
+            .foregroundColor(.clear)
+            .frame(width: 320, height: 227)
+            .background(Color.cinzaFiltro)
+            .cornerRadius(5)
+            .padding(.top, 10)
+        ZStack{
+            Text("Quarto Standard")
+                .font(
+                    Font.custom("Rounded Mplus 1c", size: 20)
+                        .weight(.heavy)
+                )
+                .foregroundColor(.black)
+                .frame(width: 218, height: 30, alignment: .topLeading)
+                .padding(.top, -230)
+                .padding(.leading, -60)
+            
+            Text("R$ 350.00") // preço variável
+                .font(
+                    Font.custom("Poppins", size: 20)
+                        .weight(.heavy)
+                )
+                .foregroundColor(.black)
+                .frame(width: 218, height: 30, alignment: .topLeading)
+                .padding(.top, -200)
+                .padding(.leading, -60)
+            
+            Button(action: { print("Botao subtrair clicado")}){
+                Rectangle()
+                    .foregroundColor(.clear)
+                    .frame(width: 50, height: 35)
+                    .background(Color.cinzaOpcaoCartao)
+                    .cornerRadius(10)
+            }
+              .padding(.top, -220)
+              .padding(.leading, 130)
+            Text("-")
+                .font(
+                    Font.custom("Poppins", size: 24)
+                        .weight(.heavy)
+                )
+                .foregroundColor(.cinzaTexto)
+                .frame(width: 218, height: 30, alignment: .topLeading)
+                .padding(.top, -220)
+                .padding(.leading, 323)
+            Button(action: { print("Botao somar clicado")}){
+                Rectangle()
+                    .foregroundColor(.clear)
+                    .frame(width: 50, height: 35)
+                    .background(Color.cinzaOpcaoCartao)
+                    .cornerRadius(10)
+            }
+              .padding(.top, -220)
+              .padding(.leading, 240)
+            Text("+")
+                .font(
+                    Font.custom("Poppins", size: 24)
+                        .weight(.heavy)
+                )
+                .foregroundColor(.cinzaTexto)
+                .frame(width: 218, height: 30, alignment: .topLeading)
+                .padding(.top, -220)
+                .padding(.leading, 445)
+            
+            Rectangle()
+              .foregroundColor(.clear)
+              .frame(width: 35, height: 35)
+              .background(Color.iconeCartaoCreditoExp)
+              .cornerRadius(10)
+              .padding(.top, -220)
+              .padding(.leading, 180)
+            Text("1")
+                .font(
+                    Font.custom("Poppins", size: 24)
+                        .weight(.heavy)
+                )
+                .foregroundColor(.white)
+                .frame(width: 218, height: 30, alignment: .center)
+                .padding(.top, -217)
+                .padding(.leading, 180)
+            VStack{
+                Image(systemName: "wifi")
+                    .frame(width: 218, height: 30, alignment: .center)
+                    .foregroundColor(Color.cinzaTexto)
+                    .frame(width: 36, height: 22, alignment: .topLeading)
+                    .padding(.top, -130)
+                    .padding(.leading, -230)
+               
+                Text("Wi-Fi")
+                  .font(Font.custom("Poppins", size: 12))
+                  .foregroundColor(Color.cinzaTexto)
+                  .frame(width: 36, height: 22, alignment: .topLeading)
+                  .padding(.top, -130)
+                  .padding(.leading, -100)
+                
+                Image(systemName: "pawprint")
+                    .frame(width: 218, height: 30, alignment: .center)
+                    .foregroundColor(Color.cinzaTexto)
+                    .frame(width: 36, height: 22, alignment: .topLeading)
+                    .padding(.top, -120)
+                    .padding(.leading, -230)
+               
+                Text("Permissão para animais")
+                  .font(Font.custom("Poppins", size: 12))
+                  .foregroundColor(Color.cinzaTexto)
+                  .frame(width: 336, height: 22, alignment: .topLeading)
+                  .padding(.top, -120)
+                  .padding(.leading, 140)
+                
+                Image(systemName: "tv")
+                    .frame(width: 218, height: 30, alignment: .center)
+                    .foregroundColor(Color.cinzaTexto)
+                    .frame(width: 36, height: 22, alignment: .topLeading)
+                    .padding(.top, -106)
+                    .padding(.leading, -230)
+               
+                Text("Tv Smart")
+                  .font(Font.custom("Poppins", size: 12))
+                  .foregroundColor(Color.cinzaTexto)
+                  .frame(width: 336, height: 22, alignment: .topLeading)
+                  .padding(.top, -110)
+                  .padding(.leading, 140)
+                
+                Image(systemName: "bathtub")
+                    .frame(width: 218, height: 30, alignment: .center)
+                    .foregroundColor(Color.cinzaTexto)
+                    .frame(width: 36, height: 22, alignment: .topLeading)
+                    .padding(.top, -96)
+                    .padding(.leading, -230)
+               
+                Text("Banheira com hidromassagem")
+                  .font(Font.custom("Poppins", size: 12))
+                  .foregroundColor(Color.cinzaTexto)
+                  .frame(width: 336, height: 22, alignment: .topLeading)
+                  .padding(.top, -100)
+                  .padding(.leading, 140)
+                Button(action: { print("Botao remover clicado")}){
+                    Text("Remover")
+                        .font(Font.custom("Poppins", size: 13))
+                        .foregroundColor(Color.laranjaEscuroEstrela)
+                }
+                  .padding(.top, -80)
+                  .padding(.leading, 220)
+               
+            }.padding(.top, -25)
+            
+        }
+    }
+}
+#Preview {
+    CardEscolha()
+}
