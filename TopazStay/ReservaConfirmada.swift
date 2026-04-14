8fd8c2ee01dad4232d7b0be2bfe1e792f03ca505
MMDDKK6500 <joao.dnwandermuren@senacsp.edu.br>
2026-04-14 16:58:35 -0300

    tela reserva confirmada

---
+++ TopazStay/ReservaConfirmada.swift
@@ -0,0 +1,40 @@
+//
+//  ReservaConfirmada.swift
+//  TopazStay
+//
+//  Created by JOAO DUQUE NARDELLI WANDERMUREN on 14/04/26.
+//
+
+import SwiftUI
+
+struct ReservaConfirmada: View {
+    var body: some View {
+        VStack {
+            Text("Reserva Confirmada!")
+                .font(Font.custom("Poppins-Medium", size: 20))
+                .padding(.bottom, 15)
+            
+            Button("OK", action: doSomething)
+                .font(Font.custom("Poppins-Regular", size: 20))
+                .foregroundStyle(Color.white)
+                .padding(.vertical, 10)
+                .padding(.horizontal, 30)
+                .background(
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color.bordaCartaoBotaoPopup)
+                )
+        }
+        .padding(20)
+        .background(
+            RoundedRectangle(cornerRadius: 10)
+                .fill(Color.white)
+                .shadow(color: .black.opacity(0.25), radius: 2, x: 0, y: 1)
+        )
+        
+        
+    }
+}
+
+#Preview {
+    ReservaConfirmada()
+}
